87594b8b24596c47f51d8383f3c1fc7a6fc9d5d9
Manuel Duarte  <carlosd99m@gmail.com>
2022-11-08 11:00:44 -0600

    Initial Commit

---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by Don Manuel on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
